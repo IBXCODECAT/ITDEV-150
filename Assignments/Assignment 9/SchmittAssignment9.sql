99064a36e5d8d59629320212d706ba612fc667d7
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-04-16 14:11:02 -0500

    Submit Assignment

--- Assignments/Assignment 9/SchmittAssignment9.sql
+++ Assignments/Assignment 9/SchmittAssignment9.sql
@@ -1,10 +1,10 @@
--------Set SQL Server Enviroment-------
+-------SET SQL SERVER ENV-------
 
 --Use the master database so we can alter or drop the MyWebDB database later if needed
 USE master
 GO
 
--------Reset Test Database-------
+-------RESET TEST DATABASE-------
 
 --Drop the MyWebDB database if it already exists for testing purposes
 IF EXISTS (SELECT name FROM master.sys.databases WHERE name = N'MyWebDB')
@@ -19,8 +19,7 @@ GO -- Wait for the database to be loaded
 
 -------Setup Database Functions-------
 
-IF EXISTS
-(
+IF EXISTS (
     SELECT *
     FROM sys.objects
     WHERE object_id = OBJECT_ID(N'dbo.GetHomeFileName') OR object_id = OBJECT_ID(N'dbo.GetClydeFileName')
@@ -45,7 +44,7 @@ RETURNS varchar(max) AS
 	END
 GO
 
--------Create Database Tables-------
+-------CREATE DATABASE TABLES-------
 
 CREATE TABLE Users (
 	UserID bigint PRIMARY KEY NOT NULL IDENTITY(1, 1), 
@@ -65,7 +64,7 @@ CREATE TABLE Downloads (
 	UserID bigint FOREIGN KEY REFERENCES Users(UserID) NOT NULL,
 )
 
--------Insert Test Data-------
+-------INSERT TEST DATA-------
 
 INSERT INTO Users (EmailAddress, FirstName, LastName) 
 VALUES 
@@ -78,7 +77,6 @@ VALUES
 	('Home V3'),
 	('Nitro-Onboarding V4')
 
-
 INSERT INTO Downloads (DownloadDate, FileName, ProductID, UserID)
 VALUES
 	(GETDATE(), dbo.GetHomeFileName(), 2, 1),
@@ -87,3 +85,38 @@ VALUES
 
 -------SELECT AND JOIN DATA FOR EASY VIEWING-------
 
+SELECT 
+	JOIN_U.EmailAddress,
+	JOIN_U.FirstName,
+	JOIN_U.LastName,
+	BASE_D.DownloadDate,
+	BASE_D.FileName,
+	JOIN_P.ProductName
+FROM Downloads AS BASE_D
+INNER JOIN Products AS JOIN_P ON BASE_D.UserID=JOIN_P.ProductID
+INNER JOIN Users AS JOIN_U ON BASE_D.UserID=JOIN_U.UserID
+ORDER BY EmailAddress DESC
+
+-------ALTER TABLES-------
+
+--Add columns to products table
+ALTER TABLE Products
+ADD Price DECIMAL(5, 2) DEFAULT 9.99
+ALTER TABLE Products
+ADD DateAdded datetime
+
+--Update the users table with a memory allocation limit
+ALTER TABLE Users
+ALTER COLUMN FirstName varchar(20) NOT NULL
+
+-------ATTEMPT TO UPDATE WITH FAIL CONDITIONS-------
+
+--Attempt to insert a null value into first name (THIS SHOULD FAIL)
+UPDATE Users 
+SET FirstName = NULL
+WHERE Users.UserID = 1
+
+--Attempt to insert a value greater than the 20 bytes allocated
+UPDATE Users
+Set FirstName = '01234567890123456789-OVERFLOW'
+WHERE Users.UserID = 1
